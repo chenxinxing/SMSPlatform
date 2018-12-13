714734c43be558cb65e04eb59508724354b76d43
bmllx <l2xiang@yeah.net>
2018-12-13 17:58:13 +0800

    修复页面bug

--- src/main/java/com/bright/dev/controller/SendController.java
+++ src/main/java/com/bright/dev/controller/SendController.java
@@ -37,18 +37,18 @@ public class SendController {
     @RequestMapping(value = "/sendsms", method = RequestMethod.POST)
     @ResponseBody
     public String send(@RequestParam("sendTo") String sendTo,@RequestParam("courierName") String courierName,
-                       @RequestParam("address") String address,@RequestParam("authCode") String authCode,HttpSession session) {
+                       @RequestParam("authCode") String authCode,HttpSession session) {
         int error = -1;
         try {
             if (session.getAttribute("loginFlag")!=null){
-                int templateId = 240484;
+                int templateId = 242825;
                 String smsSign = "素颜三部曲一麦吉丽";
                 if (sendTo != null && !"".equals(sendTo) && courierName != null && !"".equals(courierName)
-                        && address != null && !"".equals(address) && authCode != null && !"".equals(authCode)) {
-                    String[] param = new String[]{courierName, address,authCode};
+                         && authCode != null && !"".equals(authCode)) {
+                    String[] param = new String[]{courierName,authCode};
                     error = SmsDemo.sendUtil(sendTo, templateId, smsSign, param);
                     //error = 0;
-                    String smsContent = "【素颜三部曲一麦吉丽】您的 "+courierName+" 包裹已到曹集东头张敏超市斜对面 "+address+" ，请尽快来取，取件码是"+authCode+"，请联系电话18800394093.";
+                    String smsContent = "【素颜三部曲一麦吉丽】您的 "+courierName+" 包裹已到曹集东头张敏超市斜对面王杰装饰，请尽快来取，取件码是"+authCode+"，请联系电话18800394093.";
                     SendList sendList = new SendList();
                     sendList.setListId(String.valueOf(id.nextId()));
                     sendList.setSendStatus(String.valueOf(error));
@@ -69,7 +69,7 @@ public class SendController {
             error = -1;
             e.printStackTrace();
         }
-        if (error != 0 || error == -1) {
+         if (error != 0 || error == -1) {
             //session.setAttribute("error","发送失败，错误代码是："+error);
             return "fail";
         } else{
